12ecfa0df259445cc64e3bd80eb9100e257f3548
Prekrim <jonalass_karlsson@hotmail.com>
2014-09-10 20:50:06 +0200

    Plockade ut lite funktioner. 2014-09-10

--- abstraktion/kod/db.c
+++ abstraktion/kod/db.c
@@ -1,18 +1,59 @@
+// Inkludera div biblotek
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Definiera nod
 typedef struct node{
   char *key;
   char *value;
   struct node *next;
 } *Node;
 
+
 void readline(char *dest, int n, FILE *source){
   fgets(dest, n, source);
   int len = strlen(dest);
   if(dest[len-1] == '\n')
     dest[len-1] = '\0';
+}
+
+int chooseIndex(){
+  int choice;
+  
+  puts("Please choose an operation");
+  puts("1. Query a key");
+  puts("2. Update an entry");
+  puts("3. New entry");
+  puts("4. Remove entry");
+  puts("5. Print database");
+  puts("0. Exit database");
+  printf("? ");
+  scanf("%d", &choice);
+  while(getchar() != '\n'); // Clear stdin
+  
+  return (choice);  
+}
+
+void query(char buffer, Node list){
+ 
+  printf("Enter key: ");
+  readline(&buffer, 128, stdin);
+  puts("Searching database...\n");
+  int found = 0;
+  Node cursor = list;
+  while(!found && cursor != NULL){
+    if(strcmp(&buffer, cursor->key) == 0){
+      puts("Found entry:");
+      printf("key: %s\nvalue: %s\n", cursor->key, cursor->value);
+          found = 1;
+    }else{
+      cursor = cursor->next;
+    }
+  }
+  if(!found){
+    printf("Could not find an entry matching key \"%s\"!\n", &buffer);
+  }
 }
 
 int main(int argc, char *argv[]){
@@ -44,44 +85,23 @@ int main(int argc, char *argv[]){
     newNode->value = malloc(strlen(buffer) + 1);
     strcpy(newNode->value, buffer);
     newNode->next = list;
-    list = newNode;
+    list = newNode ;
   }
   // Main loop
-  int choice = -1;
+  //int choice = -1;
+  
+  // Choose the desired operatio
+  int choice = chooseIndex(); 
+  int found;
+  Node cursor;
+
   while(choice != 0){
-    puts("Please choose an operation");
-    puts("1. Query a key");
-    puts("2. Update an entry");
-    puts("3. New entry");
-    puts("4. Remove entry");
-    puts("5. Print database");
-    puts("0. Exit database");
-    printf("? ");
-    scanf("%d", &choice);
-    while(getchar() != '\n'); // Clear stdin
-    int found;
-    Node cursor;
+
     switch(choice){
+
     case 1:
-      // Query
-      printf("Enter key: ");
-      readline(buffer, 128, stdin);
-      puts("Searching database...\n");
-      found = 0;
-      cursor = list;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          puts("Found entry:");
-          printf("key: %s\nvalue: %s\n", cursor->key, cursor->value);
-          found = 1;
-        }else{
-          cursor = cursor->next;
-        }
-      }
-      if(!found){
-        printf("Could not find an entry matching key \"%s\"!\n", buffer);
-      }
-      break;
+      query(buffer, list);
+      
     case 2:
       // Update
       printf("Enter key: ");
@@ -184,6 +204,8 @@ int main(int argc, char *argv[]){
       puts("Could not parse choice! Please try again");
     }
     puts("");
+    choice = chooseIndex();
+
   }
   return 0;
 }
